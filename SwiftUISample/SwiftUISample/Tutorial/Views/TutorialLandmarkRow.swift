dc28a6b3754ca0dfd77d066de5859186db772c26
TsukasaHasegawa <asakust.awagesah@gmail.com>
2024-04-03 00:14:49 +0900

    Section1 Mark favorite landmarks

--- SwiftUISample/SwiftUISample/Tutorial/Views/TutorialLandmarkRow.swift
+++ SwiftUISample/SwiftUISample/Tutorial/Views/TutorialLandmarkRow.swift
@@ -16,6 +16,10 @@ struct TutorialLandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
